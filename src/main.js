b32cae97a80e1c71a3e2901c06ec3d60120d0379
Luiz Antônio da Silva Júnior <ikkijr@hotmai.com>
2017-08-18 19:24:53 -0300

    Foi adicionado o firebase

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import firebase from 'firebase/app'
 
 Vue.config.productionTip = false
 
@@ -11,5 +12,21 @@ new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App }
+  components: { App },
+  mounted: function () {
+    this.iniciarFirebase()
+  },
+  methods: {
+    iniciarFirebase: function () {
+      var config = {
+        apiKey: 'AIzaSyAMSYk5U_GLYOnaWstQcyYMhBi5WR2TXNw',
+        authDomain: 'twodo-d55f6.firebaseapp.com',
+        databaseURL: 'https://twodo-d55f6.firebaseio.com',
+        projectId: 'twodo-d55f6',
+        storageBucket: '',
+        messagingSenderId: '898555721971'
+      }
+      firebase.initializeApp(config)
+    }
+  }
 })
