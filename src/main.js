9f7bdda5237c3377e64f9daa70bf37f6aeba3e66
Luiz Antônio da Silva Júnior <ikkijr@hotmai.com>
2017-08-18 20:06:02 -0300

    Foi realizada a configuração do firebase

--- src/main.js
+++ src/main.js
@@ -3,14 +3,36 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import firebase from 'firebase/app'
+import * as firebase from 'firebase/app'
+import 'firebase/database'
+import 'firebase/auth'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
 
 Vue.config.productionTip = false
+
+const store = new Vuex.Store({
+  state: {
+    firebase: null
+  },
+  mutations: {
+    iniciarFirebase (state, firebase) {
+      state.firebase = firebase
+    }
+  },
+  getters: {
+    firebase: state => {
+      return state.firebase
+    }
+  }
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App },
   mounted: function () {
@@ -26,7 +48,7 @@ new Vue({
         storageBucket: '',
         messagingSenderId: '898555721971'
       }
-      firebase.initializeApp(config)
+      this.$store.commit('iniciarFirebase', firebase.initializeApp(config))
     }
   }
 })
